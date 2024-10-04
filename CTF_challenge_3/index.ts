df3b73eac4b7a012d253e0afceaa0b0ec33657dd
dilshaan_momin <144602365+md-d-cdr-4304@users.noreply.github.com>
2024-10-05 01:48:23 +0530

    ALGORAND_CAP_THE_FLAG_2024
    Each CTF challenge had a unique way of solving it and implementing it programmatically.
    
    In here you will be able to find the executable codes to complete each CTF challenge after cracking the logic of mentally solving the CTF challenge.
    
    Thank You.
    
    Hope this Helps....!

---
+++ CTF_challenge_3/index.ts
@@ -0,0 +1,38 @@
+import * as algosdk from "algosdk";
+import { my_mnemonic as mnemonic } from "../config"; // Import mnemonic from a config file
+
+(async () => {
+  const account = algosdk.mnemonicToSecretKey(mnemonic); // Convert mnemonic to an account object
+
+  // Connect to Algorand's testnet using Algonode
+  const algodClient = new algosdk.Algodv2(
+    "a".repeat(64), // Token placeholder
+    "https://testnet-api.algonode.cloud", 
+    443
+  );
+
+  const secretRecipient = "2JAZQO6Z5BCXFMPVW2CACK2733VGKWLZKS6DGG565J7H5NH77JNHLIIXLY";
+
+  const encoder = new TextEncoder();
+
+  // Create a payment transaction (sending 1 Algo)
+  const payTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    from : account.addr,
+    to : secretRecipient,
+    amount: algosdk.algosToMicroalgos(1),
+    suggestedParams: await algodClient.getTransactionParams().do(),
+    note: encoder.encode(
+      // "ae6ebfcff2e1d10cafb51c0e4ab77da22838436dd3b5d82b7d2eb6104bd30dc3" // Some transaction metadata
+      "b966c3d07e17b9442740dd2386e6e1ab191d51e964cee3b4dfc122f0fa865d10"
+    ), // Some transaction metadata
+  });    
+
+
+  const signedPayTxn = payTxn.signTxn(account.sk); // Sign the transaction
+
+  await algodClient.sendRawTransaction(signedPayTxn).do(); // Send to blockchain
+
+  const res = await algosdk.waitForConfirmation(algodClient, payTxn.txID(), 3); // Wait for confirmation
+
+  console.log(res); // Output the result
+})();
